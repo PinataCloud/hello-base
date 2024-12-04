c445c0be10f1309f73c7a86f78bb65b11a30c4a3
Steve <stevedsimkins@gmail.com>
2024-12-04 12:50:54 -0500

    chore: Updated links

--- lib/links.ts
+++ lib/links.ts
@@ -2,8 +2,8 @@ export const links = [
 	{ url: "https://docs.base.org", name: "Base Documentation" },
 	{ url: "https://www.coinbase.com/wallet", name: "Get Coinbase Wallet" },
 	{
-		url: "https://pinata.cloud/blog/how-to-mint-nfts-with-foundry-viem-and-pinata/",
-		name: "Foundational Tools",
+		url: "https://bit.ly/3D0geW3",
+		name: "Fullstack Tooling",
 	},
 	{ url: "https://discord.com/invite/buildonbase", name: "Base Discord" },
 ];
